be130b5635601ea26b352b97699fd07ce8b6a19a
Elie makoda Kowo <120121029+eliemakoda@users.noreply.github.com>
2025-01-20 12:07:52 +0100

    new changes in design and app url

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const { GenerateClient } = require("./client");
 const { GenerateAdmin } = require("./admin");
 const { GenerateApp } = require("./api");
+const cors = require("cors");
+
 // const server = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
@@ -14,7 +16,11 @@ const adminApp = GenerateAdmin(app);
 const clientApp = GenerateClient(app);
 // server.use("/api", app);
 // server.use("/", clientApp);
-
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 server.use((err, req, res, next) => {
   console.error("Unhandled error:", err.message);
   res.status(500).send({ msg: "An internal server error occurred." });
